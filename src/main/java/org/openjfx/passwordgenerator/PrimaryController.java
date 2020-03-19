b6ea5e4e5def09e5fda247b383f2d23c18eab2b1
Jasper.Wieringa <jasper.wieringa@arup.com>
2020-03-19 23:26:47 +0100

    Setting up the styling

--- src/main/java/org/openjfx/passwordgenerator/PrimaryController.java
+++ src/main/java/org/openjfx/passwordgenerator/PrimaryController.java
@@ -5,13 +5,13 @@ import java.io.IOException;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 
 public class PrimaryController {
     ObservableList<String> passwordType = FXCollections.observableArrayList("Password", "Passphrase");
 
     @FXML
-    private ChoiceBox<String> passwordBox = new ChoiceBox<String>();
+    private ComboBox<String> passwordBox = new ComboBox<String>();
 
     @FXML
     private void initialize() {
