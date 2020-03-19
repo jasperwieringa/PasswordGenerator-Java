6c4618f1c56465ed568b37b927a2f043cf470fcc
Jasper.Wieringa <jasper.wieringa@arup.com>
2020-03-19 22:57:52 +0100

    Uploading a working version

--- src/main/java/org/openjfx/passwordgenerator/PrimaryController.java
+++ src/main/java/org/openjfx/passwordgenerator/PrimaryController.java
@@ -1,22 +1,26 @@
 package org.openjfx.passwordgenerator;
 
 import java.io.IOException;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 
 public class PrimaryController {
+    ObservableList<String> passwordType = FXCollections.observableArrayList("Password", "Passphrase");
+
+    @FXML
+    private ChoiceBox<String> passwordBox = new ChoiceBox<String>();
+
     @FXML
     private void initialize() {
-        
+        passwordBox.setValue("Password");
+        passwordBox.setItems(passwordType);
     }
-
 
     @FXML
     private void clickMe() throws IOException {
         System.out.println("Hey you!");
-    };
-
-    @FXML
-    private void getChoice() throws IOException {
-        System.out.println(this);
     };
 }
