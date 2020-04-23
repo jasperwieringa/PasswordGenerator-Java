242e42ffb3fe781f98fd989d3ad86dfed1ac99a3
Jasper.Wieringa <jasper.wieringa@arup.com>
2020-04-23 21:50:36 +0200

    minor changes to the tests

--- src/test/java/org/openjfx/passwordgenerator/ControllerTest.java
+++ src/test/java/org/openjfx/passwordgenerator/ControllerTest.java
@@ -24,23 +24,23 @@ import java.util.Hashtable;
 public class ControllerTest {
 
   public ControllerTest() {
-  }
+  };
 
   @BeforeAll
   public static void setUpClass() {
-  }
+  };
 
   @AfterAll
   public static void tearDownClass() {
-  }
+  };
 
   @BeforeEach
   public void setUp() {
-  }
+  };
 
   @AfterEach
   public void tearDown() {
-  }
+  };
 
   @Test // IllegalArgumentException vanwege de testRules
   public void testPassword_wo_length_wo_rules() throws IOException {
@@ -50,7 +50,7 @@ public class ControllerTest {
 
     Assertions.assertThrows(IllegalArgumentException.class,
         () -> testPassword.generatePassword(testLength.getLength(), testRules.getRules()));
-  }
+  };
 
   @Test // IllegalArgumentException vanwege de testLength
   public void testPassword_wo_length_w_rules() throws IOException {
@@ -66,7 +66,23 @@ public class ControllerTest {
 
     Assertions.assertThrows(IllegalArgumentException.class,
         () -> testPassword.generatePassword(testLength.getLength(), testRules.getRules()));
-  }
+  };
+
+  @Test // IllegalArgumentException vanwege alle checkboxes als false
+  public void testPassword_w_length_w_rules_false() throws IOException {
+    Password testPassword = new Password();
+    PasswordLength testLength = new PasswordLength(5);
+    PasswordRules testRules = new PasswordRules(new Hashtable<String, String>());
+
+    testRules.addRules("type", "PaSSwoRD");
+    testRules.addRules("upper", "false");
+    testRules.addRules("lower", "false");
+    testRules.addRules("numberic", "false");
+    testRules.addRules("special", "false");
+
+    Assertions.assertThrows(IllegalArgumentException.class,
+        () -> testPassword.generatePassword(testLength.getLength(), testRules.getRules()));
+  };
 
   @Test // Pass
   public void testPassword_w_length_w_rules_true() throws IOException {
@@ -82,8 +98,8 @@ public class ControllerTest {
 
     testPassword.generatePassword(testLength.getLength(), testRules.getRules());
 
-    assertFalse(testPassword.getPassword().isEmpty());
-  }
+    assertTrue(!testPassword.getPassword().isEmpty());
+  };
 
   @Test // Pass
   public void testPassword_w_length_w_rules_true1() throws IOException {
@@ -99,22 +115,6 @@ public class ControllerTest {
 
     testPassword.generatePassword(testLength.getLength(), testRules.getRules());
 
-    assertFalse(testPassword.getPassword().isEmpty());
-  }
-
-  @Test // Pass
-  public void testPassword_w_length_w_rules_false() throws IOException {
-    Password testPassword = new Password();
-    PasswordLength testLength = new PasswordLength(5);
-    PasswordRules testRules = new PasswordRules(new Hashtable<String, String>());
-
-    testRules.addRules("type", "PaSSwoRD");
-    testRules.addRules("upper", "false");
-    testRules.addRules("lower", "false");
-    testRules.addRules("numberic", "false");
-    testRules.addRules("special", "false");
-
-    Assertions.assertThrows(IllegalArgumentException.class,
-        () -> testPassword.generatePassword(testLength.getLength(), testRules.getRules()));
-  }
+    assertTrue(!testPassword.getPassword().isEmpty());
+  };
 }
